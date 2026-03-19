c60b815121b4cf9a78450a730bb556db2ef32194
Xlaez <utibeabasiekong2@gmail.com>
2026-03-19 14:48:43 -0700

    Update runtime.exs

--- config/runtime.exs
+++ config/runtime.exs
@@ -72,5 +72,28 @@ if config_env() == :prod do
   #
   # Check `Plug.SSL` for all available options in `force_ssl`.
 
+    # Database
+  database_url =
+    System.get_env("DATABASE_URL") ||
+      raise """
+      environment variable DATABASE_URL is missing.
+      For example: ecto://USER:PASS@HOST/DATABASE
+      """
+
+  config :y_repo, YRepo.Repo,
+    url: database_url,
+    pool_size: String.to_integer(System.get_env("POOL_SIZE", "10"))
+    # socket_options: [:inet6]
+
+  # Redis
+  redis_url =
+    System.get_env("REDIS_URL") ||
+      raise """
+      environment variable REDIS_URL is missing.
+      For example: redis://localhost:6379/0
+      """
+
+  config :y_repo, :redis, url: redis_url
+
   config :y, :dns_cluster_query, System.get_env("DNS_CLUSTER_QUERY")
 end
